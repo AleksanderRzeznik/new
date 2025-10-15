24c314ddaab11e0c4050b81d91e84e15d727fa83
AleksanderRzeznik <aleksanderrzeznik305@gmail.com>
2025-10-15 15:42:09 +0200

    Add files via upload

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,60 +1,67 @@
+Napisz zapytanie SQL, które wyświetli z tabeli produkty nazwę produktu
+oraz cenę sprzedaży tych produktów, których cena sprzedaży jest większa
+niż 5000.
 
+SELECT nazwa_produktu , cena_sprzedazy FROM produkty WHERE cena_sprzedazy > 5000;
 
-Zadanie 71: Wyświetl wszystkie wiersze z tabeli pracownicy.
-SELECT * FROM 'pracownicy';
-Zadanie 72: Wyświetl imiona, nazwiska i stanowiska wszystkich pracowników z tabeli
-pracownicy.
-SELECT 'imie','nazwisko','stanowisko' FROM pracownicy;
-Zadanie 77: Wyświetl wszystkie dane pracowników o nazwisku Kowalski.
-SELECT * FROM pracownicy WHERE nazwisko = 'Kowalski';
-Zadanie 78: Wyświetl wszystkie dane pracowników o płacach poniżej 1600zł.
-SELECT * FROM pracownicy WHERE placa > 1600;
-Zadanie 79: Wyświetl wszystkie dane pracowników o płacach od 1400zł do 1600zł.
-SELECT * FROM pracownicy WHERE placa BETWEEN 1400 AND 1600;
-Zadanie 80: Wyświetl wszystkie dane pracowników o identyfikatorach 3 lub 5 lub 7.
-SELECT * FROM pracownicy WHERE id = 3 OR id = 5 OR id = 7;
-Zadanie 82: Wyświetl wszystkie dane pracowników, których imiona zaczynają się od ciągu
-Ka.
-SELECT * FROM pracownicy WHERE imie LIKE 'Ka%';
-Zadanie 83: Wyświetl identyfikatory, imiona i nazwiska pracowników, dla których baza nie
-zawiera numeru PESEL.
-SELECT imie,nazwisko FROM pracownicy WHERE pesel IS null;
-Zadanie 84: Wyświetl nazwiska wszystkich pracowników.
-SELECT nazwisko FROM pracownicy;
-Zadanie 85: Wyświetl wszystkie dane pracowników, którzy spełniają jednocześnie
-następujące warunki: zarabiają więcej niż 1400 zł, pracują na stanowiskach innych niż
-Kierownik oraz znany jest ich numer PESEL.
-SELECT * FROM pracownicy WHERE placa > 1400 AND NOT stanowisko = 'Kierownik' AND pesel IS NOT null;
+Napisz zapytanie SQL, które wyświetli z tabeli produkty nazwę produktu
+oraz minimalny stan magazynowy dla produktów, których minimalny stan
+magazynowy jest większy lub równy 10.
 
-Zadanie 86: Utwórz bazę zapytania_filmoteka i zaimportuj do niej plik filmoteka.sql.
-CREATE DATABASE zapytania_filmoteka;
-Zadanie 87: Wyświetl wszystkie filmy trwające ponad 115 minut.
-SELECT * FROM film WHERE dlugosc >115;
-Zadanie 89: Wyświetl wszystkie filmy które powstały przed 2000 rokiem.
-SELECT * FROM film WHERE rok > 2000;
-Zadanie 90: Wyświetl wszystkie filmy których nazwa zaczyna się na G
-SELECT * FROM film WHERE tytul LIKE'g%';
-Zadanie 93: Wyświetl wszystkich reżyserów którzy pochodzą z Polski lub USA.
-SELECT * FROM rezyser WHERE miejsce_urodzenia = 'Polska' OR miejsce_urodzenia = 'USA';
-Zadanie 96: Wyświetl id filmów które posiadają Oscara.
-SELECT * FROM ranking WHERE oscar > 0;
+SELECT min_stan_magazynowy , nazwa_produktu FROM produkty WHERE min_stan_magazynowy >= 10;
 
-Zadanie 97: Utwórz bazę zapytania_magazyn i zaimportuj do niej plik magazyn.sql.
+Napisz zapytanie SQL, które z tabeli produkty wybierze nazwę produktu
+oraz cenę sprzedaży produktów, których nazwa zaczyna się od słowa &quot;Samsung&quot;.
 
-Zadanie 98: Wyświetl wszystkich dostawców którzy nie pochodzą z Polski.
+SELECT nazwa_produktu, cena_sprzedazy FROM produkty WHERE nazwa_produktu LIKE 'Samsung%';
 
-Zadanie 99: Wyświetl wszystkich dostawców których id zaczyna się na literę B.
+Napisz zapytanie SQL, które z tabeli klienci wybierze imię, nazwisko
+oraz email klientów, których adres email kończy się domeną &quot;gmail.com&quot;.
 
-Zadanie 102: Wyświetl wszystkich klientów których kod pocztowy zaczyna się liczbą 4.
+SELECT imie, nazwisko, email FROM klienci WHERE email LIKE '%gmail.com';
 
-Zadanie 103: Wyświetl wszystkich klientów którzy nie posiadają adresu email.
+Napisz zapytanie SQL, które wyświetli z tabeli produkty nazwę produktu,
+cenę sprzedaży oraz opis produktów zawierających w nazwie słowo &quot;Pro&quot;.
 
-Zadanie 106: Wyświetl wszystkich autorów których imię zaczyna się literą D.
+SELECT nazwa_produktu, cena_sprzedazy FROM produkty WHERE opis LIKE '%Pro%';
 
-Zadanie 107: Wyświetl wszystkich pracowników którzy zarabiają od 3000 zł do 5000 zł.
+Napisz zapytanie SQL, które z tabeli produkty wybierze nazwę produktu
+oraz cenę sprzedaży produktów, których cena sprzedaży mieści się
+w przedziale od 1000 do 3000 włącznie.
 
-Zadanie 110: Wyświetl wszystkich pracowników których imię to Jan lub nazwisko to Nowak.
+SELECT nazwa_produktu, cena_sprzedazy FROM produkty WHERE cena_sprzedazy BETWEEN 1000 AND 3000;
 
-Zadanie 111: Wyświetl wszystkich pracowników który pracują na stanowisku o id równym 3.
+Napisz zapytanie SQL, które wyświetli z tabeli produkty nazwę produktu,
+wagę oraz jednostkę miary tych produktów, których waga jest zawarta
+w przedziale od 0.1 do 1 włącznie.
 
-Zadanie 113: Wyświetl wszystkie gatunki które kończą się na literę a.
+SELECT nazwa_produktu, waga, jednostka_miary FROM produkty WHERE waga BETWEEN 0.1 AND 1.1;
+
+Napisz zapytanie SQL, które z tabeli produkty wybierze nazwę produktu
+oraz id producenta dla produktów, których id_producenta przyjmuje wartość
+1, 2 lub 11.
+
+SELECT nazwa_produktu, id_producenta FROM produkty WHERE id_producenta = 1 OR id_producenta = 2 OR id_producenta = 11;
+
+Napisz zapytanie SQL, które wyświetli z tabeli pracownicy imię, nazwisko
+oraz stanowisko pracowników zatrudnionych na stanowiskach: Magazynier,
+Kierownik magazynu lub Sprzedawca.
+
+SELECT imie, nazwisko, stanowisko FROM pracownicy WHERE stanowisko = 'Magazynier' OR stanowisko = 'Kierownik magazynu' OR stanowisko = 'Sprzedawca';
+
+Napisz zapytanie SQL, które z tabeli produkty wybierze nazwę produktu,
+cenę sprzedaży oraz informację o dostępności dla produktów dostępnych
+(dostepny 1), których cena sprzedaży jest mniejsza niż 2000.
+
+SELECT nazwa_produktu, cena_sprzedazy, dostepny FROM produkty WHERE cena_sprzedazy < 2000;
+
+Napisz zapytanie SQL, które wyświetli z tabeli produkty nazwę produktu
+oraz pola dostepny i na_zamowienie dla produktów, które są niedostępne
+(dostepny 0) lub są dostępne wyłącznie na zamówienie (na_zamowienie 1).
+
+SELECT nazwa_produktu, dostepny, na_zamowienie FROM produkty WHERE dostepny = 0 OR na_zamowienie = 1;
+
+Napisz zapytanie SQL, które z tabeli produkty wybierze nazwę produktu
+oraz kod produktu dla produktów, których kod produktu zawiera ciąg znaków
+&quot;IP14&quot;.
+
