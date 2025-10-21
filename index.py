de110dd2e057f0fcc294adab40581235623c6c83
AleksanderRzeznik <aleksanderrzeznik305@gmail.com>
2025-10-21 12:05:46 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,23 @@
+while(True):
+    print("---Solid-Snake---")
+    print("> wybierz opcje")
+    print("1) Start")
+    print("2) opcje")
+    print("3) difficulty")
+    print("q) End")
+    xd = input("opcja nr: ")
+    if xd =='q':
+        break
+    if xd =='2':
+        print("to be continued")
+        break
+    if xd == '3':
+        print("1) easy")
+        print("2) normal")
+        print("3) hard")
+        if xd == '1':
+            break
+        if xd == '2':
+            break
+        if xd == '3':
+            break
