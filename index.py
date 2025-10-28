09e81a953544453e5157a858307f5b37d66036a9
AleksanderRzeznik <aleksanderrzeznik305@gmail.com>
2025-10-28 16:07:18 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -1,3 +1,23 @@
+import random
+def numguesser():
+    PLAYING = 1
+    zakres = int(input('Wprowadz do jakiej liczby chcesz zgadywac'))
+    losowa = random.randint(1, zakres)
+    liczba_prob = 3
+    while (PLAYING == 1):
+        if liczba_prob < 3:
+            wybor = int(input("zgadnij liczbę"))
+        
+            if wybor == random:
+                print(F"Zgadłeś ukrytą liczbę! Liczba to {losowa}")
+            PLAYING = 0
+        else:
+            liczba_prob += 1
+            print(F"Pozostało ci {3-liczba_prob} prób")
+    else:
+        print("Nie udało ci się zgadnąć")
+        PLAYING = 0
+
 while(True):
     print("---Solid-Snake---")
     print("> wybierz opcje")
@@ -6,6 +26,8 @@ while(True):
     print("3) difficulty")
     print("q) End")
     xd = input("opcja nr: ")
+    if xd =='1':
+        numguesser()
     if xd =='q':
         break
     if xd =='2':
@@ -16,8 +38,9 @@ while(True):
         print("2) normal")
         print("3) hard")
         if xd == '1':
-            break
+            numguesser()
         if xd == '2':
-            break
+            numguesser()
         if xd == '3':
-            break
+            numguesser()
+
