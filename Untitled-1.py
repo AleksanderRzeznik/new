e2183bacf6b157be2c0333316466049cb9bcde8f
AleksanderRzeznik <aleksanderrzeznik305@gmail.com>
2025-10-29 15:44:51 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+#print("Jakią zamianę chcesz wykonać?")
+#print("1) z C na F")
+#print("2)z F na C")
+#wybor = int(input("opcja:"))
+#if wybor == 1:
+#    C = int(input("Wprowadź stopień który chcesz zamienić"))
+#    F = C * 9/5 + 32
+#    print("po zamianie wychodzi: ", F)
+#elif wybor == 2:
+#    F = int(input("Wprowadź stopień który chcesz zamienić"))
+#    C = F / 9/5 - 32
+#    print("po zamianie wychodzi: ", C)
