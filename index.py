49bc29217d7fe13e9550b2144c8e5384c3b804e5
AleksanderRzeznik <aleksanderrzeznik305@gmail.com>
2025-11-18 12:05:06 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -1,46 +1,25 @@
 import random
-def numguesser():
-    PLAYING = 1
-    zakres = int(input('Wprowadz do jakiej liczby chcesz zgadywac'))
-    losowa = random.randint(1, zakres)
-    liczba_prob = 3
-    while (PLAYING == 1):
-        if liczba_prob < 3:
-            wybor = int(input("zgadnij liczbę"))
-        
-            if wybor == random:
-                print(F"Zgadłeś ukrytą liczbę! Liczba to {losowa}")
-            PLAYING = 0
-        else:
-            liczba_prob += 1
-            print(F"Pozostało ci {3-liczba_prob} prób")
-    else:
-        print("Nie udało ci się zgadnąć")
-        PLAYING = 0
-
+SCORE = 0
 while(True):
-    print("---Solid-Snake---")
-    print("> wybierz opcje")
-    print("1) Start")
-    print("2) opcje")
-    print("3) difficulty")
-    print("q) End")
-    xd = input("opcja nr: ")
-    if xd =='1':
-        numguesser()
-    if xd =='q':
-        break
-    if xd =='2':
-        print("to be continued")
-        break
-    if xd == '3':
-        print("1) easy")
-        print("2) normal")
-        print("3) hard")
-        if xd == '1':
-            numguesser()
-        if xd == '2':
-            numguesser()
-        if xd == '3':
-            numguesser()
+    x = random.randint(0,2)
 
+    opcje = ("kamień", "papier", "nożyce")
+
+    komputer = opcje[x]
+
+    wybor = input("co wyrzucasz?[kamień, papier, nożyce]: ")
+
+    wybor_lower = wybor.lower()
+
+    if wybor_lower == komputer:
+        SCORE -= 1
+        print("remis! -1")
+    elif wybor_lower == "kamień" and komputer == "papier":
+        print("przegrana, tracisz wszystkie punkty!")
+    elif wybor_lower == "papier" and komputer == "nożyce":
+        print("przegrana, tracisz wszystkie punkty!")
+    elif wybor_lower == "nożyce" and komputer == "kamień":
+        print("przegrana, tracisz wszystkie punkty!")
+    else:
+        SCORE += 1
+        print(f"Wygrana! +1 {SCORE}.")
